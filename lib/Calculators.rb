ca0edb70faea9fdebc0dcb978ae766522ed6dd4d
adeldjaballah <adeldjaballah@gmail.com>
2016-10-26 20:08:08 +0200

    Release Calculator

---
+++ lib/Calculators.rb
@@ -0,0 +1,15 @@
+
+class Calculators
+  def self.sum(a,b)
+    a.to_f + b.to_f
+  end
+  def self.subtr(a,b)
+    a.to_f - b.to_f
+  end
+  def self.mult(a,b)
+    a.to_f * b.to_f
+  end
+  def self.div(a,b)
+    a.to_f / b.to_f
+  end
+end
